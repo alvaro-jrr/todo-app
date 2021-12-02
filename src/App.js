5231b989cc441b1d1b03640b18820060e321be82
alvaro-jrr <alvarojrr79@gmail.com>
2021-12-02 09:20:23 -0400

    Added SEO Meta Tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import { IconContext } from "react-icons";
+import { TodosContextProvider } from "context/TodosContextProvider";
 import Header from "components/Header";
 import TodoForm from "components/TodoForm";
 import ListOfTodos from "components/ListOfTodos";
-import { TodosContextProvider } from "context/TodosContextProvider";
 import SelectTodos from "components/SelectTodos";
-import { IconContext } from "react-icons";
 import "./App.css";
 
 const App = () => {
